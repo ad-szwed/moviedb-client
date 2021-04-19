b5d25537a7694a89a21a93e3be9e2f7e264de9ed
ad-szwed <ad-szwed@users.noreply.github.com>
2021-04-19 16:43:12 +0100

    container added

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Container from 'react-bootstrap/Container'
 import MainView from './components/main-view/main-view';
 
 // Import statement indicating the need of bundling ./index.scss
@@ -9,7 +10,9 @@ import './index.scss'
 class movieDBapp extends React.Component {
   render() {
     return (
-      <MainView />
+      <Container>
+        <MainView />
+      </Container>
     );
   }
 }
