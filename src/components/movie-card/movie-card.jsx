d1775f0d42d87fdc63aff06ae1b8a45ae266dc6f
ad-szwed <ad-szwed@users.noreply.github.com>
2021-05-06 10:33:36 +0100

    trimming obsolete code

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import './movie-card.scss'
 
